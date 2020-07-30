62ba82c432fdb4a2a5f9e296d2777dda2b4a4155
billw2012 <billw2011@gmail.com>
2020-07-30 18:48:11 +0100

    Added reset button

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLogic : MonoBehaviour {
-    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
 }
