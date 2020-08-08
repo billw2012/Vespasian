5fe5bdfcb72931f00ab852021cdb6d0fa2472ca5
billw2012 <billw2011@gmail.com>
2020-08-08 14:24:14 +0100

    Background now scales exactly for screen size and aspect ratio

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -6,6 +6,12 @@ public class Background : MonoBehaviour
 {
     void Update()
     {
+        // Do this every time, as screen size can change, and its a very cheap calculation
+        var bl = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        var tr = Camera.main.ScreenToWorldPoint(Screen.width * Vector3.right + Screen.height * Vector3.up);
+        var worldSize = tr - bl;
+        var size = this.GetComponent<MeshFilter>().mesh.bounds.size.x;
+        this.transform.localScale = Vector3.one * Mathf.Max(worldSize.x, worldSize.y) / size;
         this.GetComponent<MeshRenderer>().material.mainTextureOffset = this.transform.position * -0.02f;
     }
 }
