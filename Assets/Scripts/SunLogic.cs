4fb7742e63643323aaaeed652c0c5e9a7b1eaef9
billw2012 <billw2011@gmail.com>
2020-08-08 21:20:48 +0100

    Use property blocks to set sun color

--- Assets/Scripts/SunLogic.cs
+++ Assets/Scripts/SunLogic.cs
@@ -5,48 +5,117 @@ using UnityEngine;
 public class SunLogic : MonoBehaviour
 {
     private Light childLight;
-    private Material mainMaterial;
-    private Material glowMaterial;
-    private Material pfxMaterial;
+    //private Material mainMaterial;
+    private MaterialPropertyBlock mainPB;
+    //private Material glowMaterial;
+    private MaterialPropertyBlock glowPB;
+    //private Material pfxMaterial;
+    private MaterialPropertyBlock pfxPB;
 
-    private static Color TweakHue(Color color, float newHue)
+    private static Color TweakHS(Color color, float newH, float newS)
     {
-        Color.RGBToHSV(color, out _, out float s, out float v);
-        return Color.HSVToRGB(newHue, s, v);
+        Color.RGBToHSV(color, out _, out _, out float v);
+        return Color.HSVToRGB(newH, newS, v);
     }
 
-    private static void TweakHue(Light light, float newHue)
+    private static void TweakHS(Light light, float newH, float newS)
     {
-        light.color = TweakHue(light.color, newHue);
+        light.color = TweakHS(light.color, newH, newS);
     }
 
-    private static void TweakHue(Material mat, float newHue)
+    private static void TweakHS(Material mat, string[] colorProps, float newH, float newS)
     {
-        mat.color = TweakHue(mat.color, newHue);
-        var colorProps = new[] {
-            "_BaseColor",
-            "_SpecColor",
-            "_EmissionColor",
-        };
         foreach(var colorProp in colorProps)
         {
-            mat.SetColor(colorProp, TweakHue(mat.GetColor(colorProp), newHue));
+            mat.SetColor(colorProp, TweakHS(mat.GetColor(colorProp), newH, newS));
         }
     }
 
-    public Color hue;
+    private static void TweakHS(MaterialPropertyBlock matPB, Material baseMaterial, string[] colorProps, float newH, float newS)
+    {
+        foreach (var colorProp in colorProps)
+        {
+            matPB.SetColor(colorProp, TweakHS(baseMaterial.GetColor(colorProp), newH, newS));
+        }
+    }
 
-    void Start()
+    // We could do this, but it doesn't produce as interesting results
+    //private Color TempToColor(float kelvin)
+    //{
+    //    var temp = kelvin / 100;
+    //    float red, green, blue;
+    //    if (temp <= 66)
+    //    {
+    //        red = 255;
+    //        green = temp;
+    //        green = 99.4708025861f * Mathf.Log(green) - 161.1195681661f;
+    //        if (temp <= 19)
+    //        {
+    //            blue = 0;
+    //        }
+    //        else
+    //        {
+    //            blue = temp - 10;
+    //            blue = 138.5177312231f * Mathf.Log(blue) - 305.0447927307f;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        red = temp - 60;
+    //        red = 329.698727446f * Mathf.Pow(red, -0.1332047592f);
+    //        green = temp - 60;
+    //        green = 288.1221695283f * Mathf.Pow(green, -0.0755148492f);
+    //        blue = 255;
+    //    }
+    //    return new Color(
+    //        Mathf.Clamp01(red / 255),
+    //        Mathf.Clamp01(green / 255),
+    //        Mathf.Clamp01(blue / 255)
+    //    );
+    //}
+    //[Tooltip("Sun Temperature in Kelvin"), Range(100f, 40000f)]
+    //public float temp = 1000;
+
+    public Color color = Color.white;
+
+    void OnValidate()
     {
         this.childLight = this.transform.Find("Light").GetComponent<Light>();
-        this.mainMaterial = this.GetComponent<MeshRenderer>().material;
-        this.glowMaterial = this.transform.Find("Glow").GetComponent<MeshRenderer>().material;
-        this.pfxMaterial= this.transform.Find("Particles").GetComponent<ParticleSystemRenderer>().material;
+        var mainRenderer = this.GetComponent<MeshRenderer>();
+        var glowRenderer = this.transform.Find("Glow").GetComponent<MeshRenderer>();
+        var pfxRenderer = this.transform.Find("Particles").GetComponent<ParticleSystemRenderer>();
 
-        Color.RGBToHSV(this.hue, out float newHue, out _, out _);
-        TweakHue(this.childLight, newHue);
-        TweakHue(this.glowMaterial, newHue);
-        TweakHue(this.mainMaterial, newHue);
-        TweakHue(this.pfxMaterial, newHue);
+        //var hue = TempToColor(this.temp);
+
+        Color.RGBToHSV(this.color, out float newH, out float newS, out _);
+        TweakHS(this.childLight, newH, newS);
+        //this.childLight.color = color;
+
+        if (this.mainPB == null)
+        {
+            this.mainPB = new MaterialPropertyBlock();
+            this.glowPB = new MaterialPropertyBlock();
+            this.pfxPB = new MaterialPropertyBlock();
+        }
+
+        TweakHS(this.mainPB, mainRenderer.sharedMaterial, new[] {
+            "_BaseColor",
+            "_SpecColor",
+            "_EmissionColor",
+        }, newH, newS);
+        TweakHS(this.glowPB, glowRenderer.sharedMaterial, new[] {
+            "_BaseColor",
+            "_SpecColor",
+            "_EmissionColor",
+        }, newH, newS);
+        TweakHS(this.pfxPB, pfxRenderer.sharedMaterial, new[] {
+            "_BaseColor",
+            "_SpecColor",
+            "_EmissionColor",
+        }, newH, newS);
+
+        mainRenderer.SetPropertyBlock(this.mainPB);
+        glowRenderer.SetPropertyBlock(this.glowPB);
+        pfxRenderer.SetPropertyBlock(this.pfxPB);
     }
 }
