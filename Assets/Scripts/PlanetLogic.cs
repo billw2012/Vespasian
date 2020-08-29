ca42b8237a538f42cd7c9930f3ce1f7bc934aa08
billw2012 <billw2011@gmail.com>
2020-08-29 13:28:31 +0100

    Merge of art, add planet axial rotation

--- Assets/Scripts/PlanetLogic.cs
+++ Assets/Scripts/PlanetLogic.cs
@@ -6,6 +6,9 @@ public class PlanetLogic : MonoBehaviour
     [Tooltip("Planet Radius (use this instead of scaling)"), Range(0, 10)]
     public float radius = 0.5f;
 
+    [Tooltip("Time to complete a full axial rotation (0 = no rotation)"), Range(-100, 100)]
+    public float dayPeriod = 0;
+
     public bool ringEnabled = false;
     public float ringRadiusFactor = 1.5f;
     public float ringWidthFactor = 0.5f;
@@ -28,6 +31,16 @@ public class PlanetLogic : MonoBehaviour
     void Start()
     {
         this.OnValidate();
+    }
+
+    void Update()
+    {
+        if (this.dayPeriod != 0)
+        {
+            var rot = this.geometry.rotation.eulerAngles;
+            this.geometry.rotation = Quaternion.Euler(rot + 360f * Vector3.forward * Time.deltaTime / this.dayPeriod);
+            // this.position.localRotation  this.dayPeriod * simTime;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
