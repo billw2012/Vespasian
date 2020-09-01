4e572d99ab286026a833f1301eae5bd50b116fd8
Saveliy Tronza <sparker_95@mail.ru>
2020-09-01 18:45:06 +0300

    Add TargetAreaTimed

---
+++ Assets/Scripts/System/TargetAreaTimed.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/*
+ * After player has been there for more than specified time, it is triggered.
+ * */
+
+public class TargetAreaTimed : MonoBehaviour
+{
+    public GameLogic gameLogic;
+    public GameObject player;
+    public GameObject circle;
+
+    public float duration = 1.0f;
+    float durationCurrent = 0;
+
+    [Tooltip("Radius of the area"), Range(0.1f, 10)]
+    public float radius = 1.0f;
+
+    SpriteRenderer spriteRenderer;
+
+    void OnValidate()
+    {
+        this.circle.transform.localScale = Vector3.one * 2.0f * this.radius;
+    }
+
+    void Start()
+    {
+        Assert.IsNotNull(this.gameLogic);
+        Assert.IsTrue(this.player != null, "Player is null");
+        Assert.IsNotNull(this.circle);
+        this.spriteRenderer = circle.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, this.player.transform.position) < this.radius)
+        {
+            this.durationCurrent += Time.deltaTime;
+            float progress = this.durationCurrent / this.duration;
+            float red = Mathf.Clamp(1.0f - progress, 0, 1);
+            float green = Mathf.Clamp(progress, 0, 1);
+            this.spriteRenderer.color = new Color(red, green, 0);
+            if (this.durationCurrent > this.duration)
+            {
+                this.gameLogic.WinGame();
+            }
+        } else
+        {
+            this.durationCurrent = 0;
+            this.spriteRenderer.color = Color.red;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, this.radius);
+    }
+}
