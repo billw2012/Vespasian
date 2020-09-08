a226b2bb4d69fe8bcfd2e27556a9a939cf0c40e6
Saveliy Tronza <sparker_95@mail.ru>
2020-09-08 03:01:40 +0300

    Scanning effect redone

--- Assets/Scripts/RadiusEffects/ScanEffect.cs
+++ Assets/Scripts/RadiusEffects/ScanEffect.cs
@@ -4,26 +4,34 @@ public class ScanEffect : RadiusEffect
 {
     [HideInInspector]
     public float scanned = 0;
-    LineRenderer scanEffect;
+    LineRenderer lineRenderer;
 
-    // TODO: factor out the scanner itself into its own component, as we need potentially need multiple ones per 
-    // ScanEffect and per RadiusEffectTarget.
-    float nextChangeStep = 0;
-    Vector3 currentScanPos = Vector3.zero;
-    Vector3 targetScanPos = Vector3.zero;
-    Vector3 currentScanVelocity = Vector3.zero;
+    // Prefab for laser sprite
+    public GameObject laserSpritePrefab;
+    GameObject laserSpriteObject;
+    SpriteRenderer laserSpriteRenderer;
+
+    float scanBeamAngle = -1;       // Animates between -1 and 1
+    float scanBeamDirection = 1;    // Alternates between -1 or 1
+    const float scanAngleSpeed = 12.0f;
 
     void Start()
     {
+        // Create laser sprite object
+        this.laserSpriteObject = Instantiate(this.laserSpritePrefab);
+        this.laserSpriteRenderer = laserSpriteObject.GetComponentInChildren<SpriteRenderer>();
+
+        // Create line renderer
         var scanEffectObject = new GameObject();
         scanEffectObject.transform.SetParent(this.transform);
 
-        this.scanEffect = scanEffectObject.AddComponent<LineRenderer>();
-        this.scanEffect.material = new Material(Shader.Find("Sprites/Default"));
+        this.lineRenderer = scanEffectObject.AddComponent<LineRenderer>();
+        this.lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        /*
         var colorGradient = new Gradient { mode = GradientMode.Blend };
         colorGradient.SetKeys(
             new GradientColorKey[] {
-                new GradientColorKey(Color.yellow, 0)
+                new GradientColorKey(Color.red, 0)
             },
             new GradientAlphaKey[] {
                 new GradientAlphaKey(0, 0),
@@ -33,45 +41,61 @@ public class ScanEffect : RadiusEffect
             }
         );
         this.scanEffect.colorGradient = colorGradient;
-        this.scanEffect.positionCount = 4;
-        this.scanEffect.startWidth = this.scanEffect.endWidth = 0;
-        this.scanEffect.enabled = false;
+        */
+        this.lineRenderer.startColor = new Color(1, 0, 0, 0.6f);
+        this.lineRenderer.endColor = new Color(1, 0, 0, 0.6f);
+        this.lineRenderer.positionCount = 2;
+        this.lineRenderer.startWidth = this.lineRenderer.endWidth = 0.1f;
+        this.lineRenderer.enabled = false;
     }
 
     protected override void Apply(RadiusEffectTarget target, float value, float heightRatio, Vector3 direction)
     {
-        float newScanned = Mathf.Min(1, this.scanned + value * 0.25f);
-        this.scanEffect.enabled = newScanned != this.scanned;
+        float newScanned = Mathf.Clamp(this.scanned + value * 0.25f, 0, 1000);
+        this.lineRenderer.enabled = newScanned != this.scanned;
 
         if (newScanned != this.scanned)
         {
-            float radius = this.effector.transform.localScale.x * 0.5f;
-
-            //var scanMoveVector = (Vector3)Vector2.Perpendicular(direction).normalized;
-            //var scanStartPos = this.effector.position + scanMoveVector * -radius;
-            //var endStartPos = this.effector.position + scanMoveVector * radius;
-            //var Vector = this.effector.position + offset - target.transform.position;
-
-            this.currentScanPos = Vector3.SmoothDamp(this.currentScanPos, this.targetScanPos, ref this.currentScanVelocity, 0.1f);
-            //var scanTarget = scanStartPos + (endStartPos - scanStartPos) * this.currentScanPos; // Mathf.PingPong(Time.time * 5, 1);
-                                                                                                //Quaternion.Euler(0, 0, Time.time * 360f) * Vector3.one * 0.6f;
-            var scanVector = (this.effector.position + this.currentScanPos - target.transform.position) + Vector3.back * radius;
-            //var relv = (scanVector - this.effector.position).normalized * radius;
-            
-            this.scanEffect.SetPositions(new[] {
-                target.transform.position,
-                target.transform.position + scanVector * 0.25f,
-                target.transform.position + scanVector * 0.95f,
-                target.transform.position + scanVector
-            });
-            this.scanEffect.endWidth = radius * 0.5f;
-            this.scanned = newScanned;
-
-            if (Time.time > this.nextChangeStep)
+            // Animate angle
+            this.scanBeamAngle = Mathf.Clamp(this.scanBeamAngle + ScanEffect.scanAngleSpeed * Time.deltaTime * this.scanBeamDirection, -1, 1);
+            if (this.scanBeamDirection > 0)
             {
-                this.targetScanPos = Vector3.one * radius * 1.5f * (Random.value - 0.5f);
-                this.nextChangeStep = Time.time + Random.value * 0.75f;
+                if (this.scanBeamAngle >= 1.0f)
+                    this.scanBeamDirection = -1.0f;
             }
+            else
+            {
+                if (this.scanBeamAngle <= -1.0f)
+                    this.scanBeamDirection = 1.0f;
+            }
+
+            // Calculate direction vectors
+            // Vector from planet to ship
+            Vector3 diff = this.effector.transform.position - target.transform.position;
+            Vector3 dir = Vector3.Normalize(diff);
+            // Vector orthogonal ot the one above
+            Vector3 norm = Vector3.Cross(dir, Vector3.forward);
+            
+            // Draw line            
+            float planetRadius = this.effector.transform.localScale.x * 0.5f;
+            Vector3[] positions =
+            {
+                target.transform.position,
+                this.effector.transform.position + planetRadius*norm*this.scanBeamAngle
+            };
+            this.lineRenderer.SetPositions(positions);
+
+            // Rotate sprite accordingly
+            this.laserSpriteRenderer.enabled = true;
+            this.laserSpriteObject.transform.position = 0.5f * (target.transform.position + this.effector.transform.position);
+            this.laserSpriteObject.transform.LookAt(this.effector.transform);
+            this.laserSpriteObject.transform.localScale = new Vector3(2*planetRadius, 2*planetRadius, diff.magnitude);
+
+            this.scanned = newScanned;
+        }
+        else
+        {
+            this.laserSpriteRenderer.enabled = false;
         }
     }
 };
