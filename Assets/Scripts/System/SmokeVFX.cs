6d45727828b5bd4ed884b7c973b61cb280567b68
billw2012 <billw2011@gmail.com>
2020-09-15 19:28:43 +0100

    Alternative hot jupiter vfx

---
+++ Assets/Scripts/System/SmokeVFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeVFX : MonoBehaviour
+{
+    // Transform of the planets parent (what it is orbiting)
+    public Transform orbitOrigin;
+    // Transform of the planet itself, including scale
+    public Transform planet;
+
+    void Update()
+    {
+        var direction = this.planet.position - this.orbitOrigin.position;
+        this.transform.rotation = Quaternion.FromToRotation(Vector2.right, direction);
+        this.transform.localScale = this.planet.localScale;
+    }
+}
