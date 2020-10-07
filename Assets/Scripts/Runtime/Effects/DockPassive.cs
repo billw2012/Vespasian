84256a2421cdc9b6d4b70e60627d9b50997b75a6
Saveliy Tronza <sparker_95@mail.ru>
2020-10-07 23:48:40 +0300

    Test docking scene

---
+++ Assets/Scripts/Runtime/Effects/DockPassive.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This is the passive docking port, to be attached to space stations
+ */
+
+public class DockPassive : EffectSource
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override Color gizmoColor => Color.red;
+    public override string debugName => "DockPassive";
+}
