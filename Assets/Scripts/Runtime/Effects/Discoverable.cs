8e0ff94698521dd4a2cad46f5e1d4b6748efe28c
Saveliy Tronza <sparker_95@mail.ru>
2020-10-18 19:27:32 +0300

    Add discoverable components

---
+++ Assets/Scripts/Runtime/Effects/Discoverable.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// Attach it to something which can be discovered by player.
+
+class Discoverable : MonoBehaviour
+{
+    private bool _discovered = false;
+
+    private Renderer[] renderers;
+
+    public float discoveryRadius = 10.0f;
+
+    [Tooltip("Transform to use as the effect source")]
+    public Transform originTransform;
+
+    public bool discovered
+    {
+        get { return this._discovered; }
+    }
+
+    private void Start()
+    {
+        this.renderers = this.GetComponentsInChildren<Renderer>();
+        Debug.Log($"Object: {this}, Renderers {this.renderers.Length}:");
+        foreach (var r in this.renderers)
+            Debug.Log($"     {r}");
+
+        this.EnableAllRenderers(false);
+
+        // Estimate discovery radius by object's size
+        var planetLogic = this.GetComponent<BodyLogic>();
+        if (planetLogic != null)
+        {
+            this.discoveryRadius = planetLogic.radius / 2.0f * 30.0f;
+        }
+
+    }
+
+    public void Discover()
+    {
+        this._discovered = true;
+        this.EnableAllRenderers(true);
+    }
+
+    void EnableAllRenderers(bool enable)
+    {
+        foreach (var rendererComponent in this.renderers)
+        {
+            rendererComponent.enabled = enable;
+            Debug.Log($"Renderer {rendererComponent} enabled: {enable}");
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Effects/DiscoverableScanner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+class DiscoverableScanner : MonoBehaviour
+{
+    private void Update()
+    {
+        var discoverables = Object.FindObjectsOfType<Discoverable>();
+        foreach (var obj in discoverables)
+        {
+            if (!obj.discovered)
+            {
+                float dist = Vector3.Distance(obj.originTransform.position, this.transform.position);
+                if (dist < obj.discoveryRadius)
+                {
+                    obj.Discover();
+                }
+            }
+        }
+    }
+}
