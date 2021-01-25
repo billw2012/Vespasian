7e7f2832fc5f5c8dbfb09e35c8cbd629c911381f
Saveliy Tronza <sparker_95@mail.ru>
2021-01-25 03:34:38 +0300

    Add line indicator when firing a rocket

--- Assets/Scripts/Runtime/UI/DragToFireWeapon.cs
+++ Assets/Scripts/Runtime/UI/DragToFireWeapon.cs
@@ -4,24 +4,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragToFireWeapon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragToFireWeapon : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField]
     private GameObject weaponOrigin = null;
     [SerializeField]
     private GameObject projectilePrefab = null;
 
-    //private bool dragging = false;
+    [SerializeField]
+    private LineRenderer fireDirectionLineRenderer;
+
+    //  We still must keep this bool,
+    // because OnDrag is not called when we have started dragging
+    // and keep dragging but don't move the mouse any more
+    private bool dragging = false;
+
+    private Vector2 lastOnDragPos;
+
+    void Start()
+    {
+        this.fireDirectionLineRenderer.enabled = false;
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        //this.dragging = true;
         Debug.Log("DragToFireWeapon: dragging started");
+        this.fireDirectionLineRenderer.enabled = true;
+        this.dragging = true;
+    }
+
+    void Update()
+    {
+        if (this.dragging)
+        {
+            var touchWorldPos = this.getCursorWorldPos(this.lastOnDragPos);
+            Vector3[] linePositions =
+            {
+                this.weaponOrigin.transform.position,
+                touchWorldPos
+            };
+            linePositions[0].z = 0;
+            linePositions[1].z = 0;
+            this.fireDirectionLineRenderer.SetPositions(linePositions);
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // eh?
+        this.lastOnDragPos = eventData.position;
+    }
+
+    private Vector3 getCursorWorldPos(Vector2 position)
+    {
+        var cursorScreenPos3D = new Vector3(position.x, position.y, 0);
+        return Camera.main.ScreenToWorldPoint(cursorScreenPos3D);
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -29,12 +65,13 @@ public class DragToFireWeapon : MonoBehaviour, IBeginDragHandler, IDragHandler, 
         Debug.Log("DragToFireWeapon: dragging ended");
 
         // Calculate the projectile direction
+        var cursorWorldPos = this.getCursorWorldPos(eventData.position);
         var weaponOriginPos = this.weaponOrigin.GetComponent<Transform>().position;
-        var weaponOriginScreenPos = (Vector2)Camera.main.WorldToScreenPoint(weaponOriginPos);
-        var dragEndScreenPos = eventData.position;
-        var shootVector = (dragEndScreenPos - weaponOriginScreenPos).normalized;
+        var shootVector = cursorWorldPos - weaponOriginPos;
+        shootVector.z = 0;
+        shootVector = shootVector.normalized;
 
-        Debug.Log($"DragToFireWeapon: weapon screen pos: {weaponOriginScreenPos}, drag end screen pos: {dragEndScreenPos}, shoot vector: {shootVector}");
+        Debug.Log($"DragToFireWeapon: shoot vector: {shootVector}");
 
         // Instantiate the projectile
         var projectile = Instantiate(this.projectilePrefab);
@@ -44,6 +81,7 @@ public class DragToFireWeapon : MonoBehaviour, IBeginDragHandler, IDragHandler, 
         projectileSimMovement.alignToVelocity = false;
         projectileSimMovement.SetPositionVelocity(weaponOriginPos, projectileRotation, (Vector2)originSimMovement.velocity);
 
-        //this.dragging = false;
+        this.dragging = false;
+        this.fireDirectionLineRenderer.enabled = false;
     }
 }
